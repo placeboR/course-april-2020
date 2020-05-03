42b77cb6031f14efb473043cbfb151187b4a0521
placeboR <d.s_ht@hotmail.com>
2020-05-03 16:41:10 -0400

    update database

--- homework/HW1/src/hw1/demo/HW1.java
+++ homework/HW1/src/hw1/demo/HW1.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class HW1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        Topic: String
 //        1)
         String str = new String("Algorithms");
@@ -22,21 +22,37 @@ public class HW1 {
         String test = new String("https://www.amazon.com/demo?test=abc");
         List<String> tokens = new ArrayList<String>();
         StringTokenizer strTokenizer = new StringTokenizer(test, ":/.?=");
-        while (strTokenizer.hasMoreTokens()){
+        while (strTokenizer.hasMoreTokens()) {
             tokens.add(strTokenizer.nextToken());
         }
-        for(String s : tokens){
+        for (String s : tokens) {
             System.out.println(s);
         }
         System.out.println();
 //        4)
         String[] strList = {"Nice", "to", "meet", "you"};
         String res = new String();
-        for(String s : strList){
+        for (String s : strList) {
             res = res + s + " ";
         }
-        res = res.substring(0, res.length()-1);
+        res = res.substring(0, res.length() - 1);
         System.out.println(res);
+        System.out.println();
+//        Topic: final
+        final double PI = 3.14;
+        finalFun(); //Notice: Cannot make a static reference to the non-static method
+        finalClass f = new finalClass();
+        System.out.println(f.incID());
+    }
+
+    static final void finalFun(){
+        System.out.println("This is a final method.");
+    }
+    static final class finalClass{
+        int id = 7;
+        public int incID(){
+            return ++id;
+        }
     }
     public static boolean cmpStr(String a, String b){
         int sz_A = a.length();
--- homework/HW1/src/hw1/tp_static/HW1_static.java
+++ homework/HW1/src/hw1/tp_static/HW1_static.java
@@ -1,4 +1,5 @@
 package hw1.tp_static;
 
 public class HW1_static {
+    
 }
